e90f41abc4a679b591fe583916d33b60dabaa559
NaeemUllah98 <naeem66241@gmail.com>
2024-04-28 18:20:57 +0500

    our products page except slider section with responsive

---
+++ src/pages/ourproducts.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ourproducts() {
+   return (
+  <>
+    <div className=' bg-[url("../images/garlic-powder.png")] bg-no-repeat bg-cover bg-bottom w-full  xxl:h-[1200px] lg:h-[550px] md:h-[450px] sm:h-[400px] min-h-[350px]'>
+    </div>
+    <div className='bg-[url("../images/elevation.png")] bg-no-repeat bg-cover md:bg-center bg-center w-full  xxl:h-[833px] lg:h-[550px] md:h-[450px] sm:h-[400px] min-h-[350px]'>
+
+    </div>
+  </>
+  )
+}
+export default ourproducts
